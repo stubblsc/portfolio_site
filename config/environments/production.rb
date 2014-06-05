8ab59ea660767a13d32a3f140ef39e83096ab1be
stubblsc <stubblsc@miamioh.edu>
2014-06-05 03:01:10 -0400

    assets:clean

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,9 +59,9 @@ MomArtSite::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['lightbox.min.js', 'lightbox.css']
 
-  # Ignore bad email addresses and do not raise email delivery errors.
+  # Ignore bad email addresses and do nbot raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   #config.action_mailer.raise_delivery_errors = true
   #config.action_mailer.perform_deliveries = true
