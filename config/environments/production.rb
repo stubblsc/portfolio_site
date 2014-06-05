9c7ed1dd03c3dae835c14268f298304790a08d14
stubblsc <stubblsc@miamioh.edu>
2014-06-04 21:16:55 -0400

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,20 @@ MomArtSite::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = {:host => "http://protected-journey-3036.herokuapp.com/"}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :authentication => :plain,
+    :user_name => "stub6225.junk"
+    :password => "stubble1"
+    :domain => "gmail.com"
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
