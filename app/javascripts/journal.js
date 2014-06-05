373460e06804322752bc9ee496bbf42f6730fcdf
stubblsc <stubblsc@miamioh.edu>
2014-06-05 11:34:13 -0400

    added ssl

--- app/javascripts/journal.js
+++
@@ -1,6 +0,0 @@
-// Journal
-// Bootswatch
-//= require jquery
-//= require jquery_ujs
-//= require journal/loader
-//= require journal/bootswatch
--- app/javascripts/journal/bootswatch.js
+++
@@ -1,3 +0,0 @@
-// Journal
-// Bootswatch
-// bootswatch.js
