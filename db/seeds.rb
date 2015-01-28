54bbc78094c12e2ae683e1507202e9365b9649d9
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2015-01-27 20:12:59 -0500

    fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+About.create!([
+  {text: "Nunc vel egestas lectus. In eu sagittis purus. Pellentesque accumsan urna ac risus faucibus iaculis. Pellentesque tincidunt, justo eu auctor convallis, ipsum orci pellentesque urna, a imperdiet ligula ex eget mi. Suspendisse vehicula condimentum cursus. Sed lobortis turpis ac nulla blandit cursus. Duis finibus magna eu auctor fringilla. Morbi pulvinar magna mauris, ut auctor felis ornare non. Nulla molestie facilisis facilisis. Cras maximus risus vel sodales bibendum. Vivamus eu facilisis metus. Duis euismod ligula euismod tempor vulputate. Sed eget ipsum posuere, mattis elit eu, condimentum lacus. Phasellus porta feugiat velit, vulputate pretium tellus pellentesque nec. In hac habitasse platea dictumst.", background_file_name: nil, background_content_type: nil, background_file_size: nil, background_updated_at: nil, header: "About..."}
+])
+Home.create!([
+  {text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultrices condimentum diam a pellentesque. Aliquam et aliquam lacus. Nullam iaculis sed mi ut laoreet. Duis pretium ac urna vel maximus. Nam placerat tellus augue, in commodo odio volutpat nec. In nisl lacus, ultricies et quam id, volutpat tincidunt nisl. Nunc efficitur sollicitudin commodo. Duis luctus dolor in neque placerat, non posuere magna finibus. Curabitur sapien ipsum, pellentesque sit amet enim et, pharetra fermentum lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum mauris sit amet tellus elementum mattis. Maecenas sed metus fermentum, posuere nisl sodales, consectetur magna. Nullam ut odio ut felis mollis tincidunt. Quisque vel volutpat est.", background_file_name: nil, background_content_type: nil, background_file_size: nil, background_updated_at: nil, header: "Welcome to....."}
+])
+Section.create!([
+  {title: "Section 1", order: 1},
+  {title: "Section 2", order: 2}
+])
