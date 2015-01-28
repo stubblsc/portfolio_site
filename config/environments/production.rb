f79e6f2ffd14f312d7e88c28e10debaaf2a23bb4
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2015-01-28 13:50:02 -0500

    added s3 support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,10 +101,11 @@ MomArtSite::Application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
 end
