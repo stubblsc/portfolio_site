850d3d359879c7a977220a05576513095ccd50d7
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2015-10-19 23:19:39 -0400

    cap mpd

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,19 +2,26 @@
 lock '3.4.0'
 
 # Change these
-server '104.236.75.163', port: 80, roles: [:web, :app, :db], primary: true
+server '104.236.75.163', roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:stubblsc/portfolio_site.git'
-set :application,     'portfolio_site'
+set :scm,             :git
+set :branch,          'master'#:master#423.to_s.to_sym
+set :application,     'picontrol'
 set :user,            'deploy'
-set :puma_threads,    [4, 16]
+set :puma_threads,    [1, 6]
 set :puma_workers,    1
+
+# set :ssh_options, {
+#   forward_agent: true,
+#   port: 4321
+# }
 
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
-set :deploy_via,      :remote_cache
+set :deploy_via,      :copy
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
@@ -27,8 +34,6 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
-set :scm,           :git
-set :branch,        :master
 set :format,        :pretty
 set :log_level,     :debug
 set :keep_releases, 5
@@ -50,16 +55,16 @@ namespace :puma do
 end
 
 namespace :deploy do
-  # desc "Make sure local git is in sync with remote."
-  # task :check_revision do
-  #   on roles(:app) do
-  #     unless `git rev-parse HEAD` == `git rev-parse origin/master`
-  #       puts "WARNING: HEAD is not the same as origin/master"
-  #       puts "Run `git push` to sync changes."
-  #       exit
-  #     end
-  #   end
-  # end
+#   desc "Make sure local git is in sync with remote."
+#   task :check_revision do
+#     on roles(:app) do
+#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
+#         puts "WARNING: HEAD is not the same as origin/master"
+#         puts "Run `git push` to sync changes."
+#         exit
+#       end
+#     end
+#   end
 
   desc 'Initial Deploy'
   task :initial do
@@ -76,7 +81,6 @@ namespace :deploy do
     end
   end
 
-  # before :hook, :install
   # before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
