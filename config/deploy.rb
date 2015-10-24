48169521d4458a9d0cd26fb177ca43a58e0d2033
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2015-10-24 18:48:48 -0400

    unicorn -> puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,8 @@ server '104.131.114.129', roles: [:web, :app, :db], primary: true
 set :repo_url,        'git@github.com:stubblsc/portfolio_site.git'
 set :application,     'portfolio_site'
 set :user,            'deployer'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :puma_threads,    [1, 4]
+set :puma_workers,    1
 
 # Don't change these unless you know what you're doing
 set :pty,             true
