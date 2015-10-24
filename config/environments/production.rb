42f61f3a65d1cebf30064308b4ff754c3daadf6f
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2015-10-24 19:30:46 -0400

    vars set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,9 +103,9 @@ MomArtSite::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => "lynn-portfolio",
+      :access_key_id => "AKIAI24I3FBNTYHU65EQ",
+      :secret_access_key => "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"
     }
   }
 end
