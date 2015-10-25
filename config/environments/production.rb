54ba7abc22b742b8fedcf9fc9b70338ee8674ae8
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2015-10-25 11:42:51 -0400

    setup env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ MomArtSite::Application.configure do
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => AWS['AWS_SECRET_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
