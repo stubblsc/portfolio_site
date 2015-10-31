bb1fced902eaf796e2d80b5c7f05b5f1eb2fb0a5
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2015-10-31 15:17:53 -0400

    added back deleted files

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  protected
+	def authenticate
+  		authenticate_or_request_with_http_basic do |username, password|
+    	username == ENV['ADMIN_USER'] && password == ENV['ADMIN_PASS']
+    	end
+  	end
+end
